8164b2645ac71028114f1c675f5ba0167045bcd5
Richard Lee <richard.lee@rivosoftware.com>
2016-01-18 23:03:46 +0000

    Refactored
    Completed refactoring and streamlining the code. Base functionality
    should now work

--- README.md
+++ README.md
@@ -3,6 +3,17 @@ UpdateZip
 
 The inspiration for this utility came when I needed to programatically change environment variables inside a jar file
 (which is just a zip file with a different name).
-As with any working software, additional requests come in and this is an attempt to properly flesh out the functionality.
+Over time additional requests came in and this is an attempt to properly flesh out that functional requirement.
+The functionality is provided by a series of commands that tell the script what to do.
+
+The commands currently supported are
+- DELETE file
+- UPDATE file newfile
+- ADD    file [path]
+- RENAME file newname
+- REGEX  file search replace [flags]
+
+For simple manipulations, a single command can be provided at the command line.
+For more powerful changes a script can to specified which list each command on a seperate line.
 
 For usage, please see the in-built help.
--- updatezip.py
+++ updatezip.py
@@ -1,6 +1,7 @@
-#!/usr/bin/env python
+﻿#!/usr/bin/env python
 "UpdateZip script to change zip files"
 
+from __future__ import print_function
 import zipfile
 import re
 import zlib
@@ -27,10 +28,8 @@ remove multiple files or a file which might change.
 Aliases: REMOVE DEL RM""",
               'U': """Command: UPDATE file newfile
 
-Adds a file from the OS putting it in path inside the zip. If a path is
-provided it will be placed inside the zip at that path. If not path is provided
-and the path is relative then the relative path is used. Otherwise the file is
-put in the root.
+Replaces a file with one from the OS. The name and path inside the zip remain
+the same as the one replaced.
 
 Aliases: REPLACE""",
              'A': """Command: ADD file [path]
@@ -59,11 +58,11 @@ Flags
 def print_help (command = None):
     "Display the help file"
     command = "main" if not command else command.lower()
-    print ("Usage: UpdateZip.py [options] zipfiles\n")
+    print("Usage: UpdateZip.py [options] zipfiles\n")
     if command in __uz_aliases:
-        print (__uz_help[__uz_aliases[command]])
+        print(__uz_help[__uz_aliases[command]])
     else:
-        print ("""Options
+        print("""Options
  -h --help   this help file, use -h command for help on individual commands
  -n --name   name of zip file (defaults to reading from stdin)
  -d --dest   destination path (defaults to the same as the input file)
@@ -148,24 +147,26 @@ so ren file newname would be ['ren','file','newname']"""
     itemsused = 0
 
     c = command[0].lower()
-    if c in __uz_aliases and len(command) > __uz_params[c]:
-        compiled.append(__uz_aliases[c])
-        if c == 'R': # Regex command
+    if c in __uz_aliases and len(command) > __uz_params[__uz_aliases[c]]:
+        c1 = __uz_aliases[c]
+        compiled.append(c1)
+        if c1 == 'R': # Regex command
             compiled.append(re.compile(command[1]))
             compiled.append(command[2])
             compiled.append(command[3])
             itemsused = 4
-        elif c == 'A': # Add command
+        elif c1 == 'A': # Add command
             compiled.append(command[1])
+            compiled.append(None) # TODO: allow overriding of name
             itemsused = 2
-        elif c == 'D': # Delete command
+        elif c1 == 'D': # Delete command
             compiled.append(re.compile(command[1]))
             itemsused = 2
-        elif c == 'U': # Update command
+        elif c1 == 'U': # Update command
             compiled.append(re.compile(command[1]))
             compiled.append(command[2])
             itemsused = 3
-        elif c == 'M': # Rename (move) command
+        elif c1 == 'M': # Rename (move) command
             compiled.append(re.compile(command[1]))
             compiled.append(command[2])
             itemsused = 3
@@ -211,14 +212,21 @@ outfile   new filename path"""
                             print("Renaming %s to %s" % (jfile,command[2]))
                             jfile = command[2]
                         elif command[0] == 'U':
-                            print("Updating %s - to be implemented" % (jfile))
+                            if os.path.isfile(command[2]):
+                                print("Updating %s with %s" % (jfile,command[2]))
+                                jarout.write(command[2],jfile)
+                                data = None
+                            else:
+                                print("Unable to update %s - file %s not found" % (jfile,command[2]))
                 if data:
                     jarout.writestr(jfile,data)
 
             # process add commands
             for command in commands:
                 if command[0] == 'A':
-                    print("Adding file %s - to be implemented" % (command[1]))
+                    if os.path.isfile(command[1]):
+                        print("Adding file %s" % (command[1]))
+                        jarout.write(command[1],command[2])
 
             jarout.close()
             jarin.close()
@@ -259,6 +267,8 @@ def main (inargs):
         used = 0
     else:
         compiled , used  = compile_command(args)
+        print(compiled)
+        print(used)
         if compiled:
             commands.append(compiled)
 
